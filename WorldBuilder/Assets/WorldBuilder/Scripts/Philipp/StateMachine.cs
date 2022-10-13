1d7c7f3b0686cf491166c8f5a646948dc462786a
Pilegrim <pilegrim60@gmail.com>
2022-10-13 12:41:49 +0200

    stateMachine & XR Rig

---
+++ WorldBuilder/Assets/WorldBuilder/Scripts/Philipp/StateMachine.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+
+    public enum State
+    {
+        Idle,
+        EditingRotation,
+        EditingScaleAllAxis,
+        EditingScaleIndividualAxis,
+        EditingTranslation
+    }
+
+    [HideInInspector] public State state;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        state = State.Idle;
+    }
+}
