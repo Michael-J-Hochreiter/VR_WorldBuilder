c85b78cd98f8e8ca5c13a18b9fd495781d763760
Michael-J-Hochreiter <michael.j.hochreiter@gmail.com>
2022-10-28 18:36:55 +0200

    added SelectionUI and mechanic for SelectionUI, added BuildingBlock

---
+++ WorldBuilder/Assets/WorldBuilder/Scripts/Michi/BuildingBlock.cs
@@ -0,0 +1,26 @@
+// AUTHOR: MICHAEL HOCHREITER
+
+using UnityEngine;
+
+public class BuildingBlock : MonoBehaviour
+{
+    public GameObject selectionUIPrefab;
+    [HideInInspector] public GameObject selectionUI;
+
+    void Start()
+    {
+        EnableSelectionUI();
+    }
+
+    public void EnableSelectionUI()
+    {
+        selectionUI = Instantiate(selectionUIPrefab);
+        selectionUI.GetComponent<SelectionUI>().buildingBlock = transform;
+    }
+
+    public void DisableSelectionUI()
+    {
+        Destroy(selectionUI);
+    }
+
+}
